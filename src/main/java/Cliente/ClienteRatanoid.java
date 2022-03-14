b57f4623ecaec8af18134166a0db43415bda0a18
JuanCarlos-Carretero <jcarretero@elpuig.xeill.net>
2022-03-14 22:34:45 +0100

    Changes by jbecerra

--- src/main/java/Cliente/ClienteRatanoid.java
+++ src/main/java/Cliente/ClienteRatanoid.java
@@ -1,5 +1,70 @@
 package Cliente;
 
-public class ClienteRatanoid {
+import Servidor.Jugador;
 
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClienteRatanoid extends Thread {
+
+    private String hostname;
+    private int port;
+    private boolean acabat = false;
+    private InputStream is;
+    private OutputStream os;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+
+    public ClienteRatanoid(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        Socket socket;
+        try {
+            socket = new Socket(InetAddress.getByName(hostname), port);
+            os = socket.getOutputStream();
+            output = new ObjectOutputStream(os);
+            is = socket.getInputStream();
+            input = new ObjectInputStream(is);
+
+            while (!acabat) {
+                Jugador jugador = new Jugador("joel", 100);
+                output.writeObject(jugador);
+                output.flush();
+
+                jugador = (Jugador) input.readObject();
+                printLlista(jugador);
+                acabat = true;
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            output.close();
+            input.close();
+            is.close();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printLlista(Jugador jugador) {
+        System.out.println(jugador.getApodo() + " " + jugador.getPuntuacion());
+    }
+
+    public static void main(String[] args) {
+        ClienteRatanoid clientObjLlista = new ClienteRatanoid("localhost", 5557);
+        clientObjLlista.start();
+    }
 }
